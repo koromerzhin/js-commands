7619447dbe457e0ad77b0f7d3ac7849c88d032a6
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2022-10-28 11:21:45 +0200

    wip: use promise

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Docker = require('dockerode');
 const fs = require('fs');
 const yaml = require('yaml');
-const child_process = require('child_process');
 const docker = new Docker();
-function getInfoContainers(data)
+
+var imagesData = [];
+async function getInfoContainers(data)
 {
   docker.listContainers({ all: true }).then(containers => {
     var end = 0;
@@ -28,24 +29,67 @@ function getInfoContainers(data)
   });
 }
 
-function dockerpull(image)
+async function getImagesLocal()
 {
-  console.log('docker pull ' + image);
-  watcher = child_process.spawn('docker', ['pull', image]);
-  watcher.stdout.on('data', (data) => {
-    console.log(`${data}`);
+  await docker.listImages().then(images => {
+    images.forEach(image => {
+      var repoTags = image.RepoTags;
+      if (repoTags != null) {
+        var tag = repoTags[0];
+        imagesData[tag] = tag
+      }
+    })
+  });
+  console.log('Images in local');
+  console.table(imagesData);
+}
+
+function execShellCommand(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+    });
   });
 }
 
-// test = {
-//   'labstag_phpfpm': 'running',
-//   'labstag_phpfpmexec': 'exited'
-// };
-// const IntervalContainer = setInterval(getInfoContainers,1000, test);
+async function readDockerCompose()
+{
+  const file = fs.readFileSync('./docker-compose.yml', 'utf8');
+  const parsing = yaml.parse(file);
+  var promises = [];
+  for (var key in parsing.services) {
+    var image = parsing.services[key].image;
+    if (imagesData[image] == undefined) {
+      console.log('docker pull ' + image);
+      await execShellCommand('docker pull ' + image);
+    
+      imagesData[image] = image;
+    }
+  }
 
-const file = fs.readFileSync('./docker-compose.yml', 'utf8');
-const parsing = yaml.parse(file);
-for (var key in parsing.services) {
-  var image = parsing.services[key].image;
-  dockerpull(image);
+  await Promise.all(promises);
 }
+
+async function waitingContainers()
+{
+  test = {
+    'labstag_phpfpm': 'running',
+    'labstag_phpfpmexec': 'exited'
+  };
+  const IntervalContainer = await setInterval(getInfoContainers,1000, test);
+}
+
+async function global() {
+  console.clear();
+  await getImagesLocal();
+  await readDockerCompose();
+  // Bug
+  await waitingContainers();
+  console.log('aa');
+}
+
+global();
