a7f773489faa99d7fe09064b1f727bd317df0731
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2022-12-22 11:32:22 +0100

    feat(deps): add npm-run-all

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const version = '1.1.4';
+const version = '1.1.5';
 const Docker = require('dockerode');
 const { exec } = require('child_process');
 const dotenvConfig = require('dotenv').config();
