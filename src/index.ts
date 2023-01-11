be4b10b370c3e34db488ffc23df1c150b5ac1c39
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-01-11 16:31:40 +0100

    fix(scripts): include folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const version = '1.2.2';
+const version = '1.2.3';
 const Docker = require('dockerode');
 const dotenvConfig = require('dotenv').config();
 const { program } = require('commander');
