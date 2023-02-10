4acf9bc9215c254555a41b3d42388a3074311b75
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-10 10:10:01 +0100

    feat(phar): updaate libs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const version = '1.2.14';
+const version = '1.2.15';
 const Docker = require('dockerode');
 const dotenvConfig = require('dotenv').config();
 const { program } = require('commander');
