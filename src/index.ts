434977a2c34de3a60f706e9c2d494a5e96b67680
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-06-21 16:25:23 +0200

    fix(libs): correction yaml

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const version = '1.2.19';
+const version = '1.2.20';
 const Docker = require('dockerode');
 const dotenvConfig = require('dotenv').config();
 const { program } = require('commander');
